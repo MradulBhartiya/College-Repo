7caf902636cb2956953ae6a5b783eb0114dbf88c
Mradul Bhartiya <anshubhartiya111@gmail.com>
2026-02-11 11:59:08 +0530

    feb 11 remaining

---
+++ 11_feb.py
@@ -0,0 +1,27 @@
+a = 15
+b = 4
+
+#Artihmatic operators
+print("Arithmatic Operators")
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+print(a/b)
+print(a**b)
+
+#Comparasion opereators
+print("\n Comparasion opereators")
+print(a == b)
+print(a <= b)
+print(a >= b)
+print(a != b)
+
+#Logical Operators
+print("\n Logical Operators")
+c = true
+d = false
+
+print(c and d)
+print(c or d)
+print(not c)
